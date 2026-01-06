c58d235b66c8f5497b07ae47b36e50c9a33c3eed
lukikuthi <lucaskikuthi@gmail.com>
2026-01-06 17:39:42 -0300

    fix vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: "/", 
   plugins: [react()],
+  base: "/Pulse/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
