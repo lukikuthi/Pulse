062799818d9c5e84de0b3e5bc0a72435e15fa746
lukikuthi <lucaskikuthi@gmail.com>
2026-01-06 17:43:44 -0300

    deploy github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
   base: "/Pulse/",
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
